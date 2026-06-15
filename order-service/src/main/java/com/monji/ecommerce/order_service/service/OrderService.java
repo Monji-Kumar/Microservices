b6138dcff6e73c380a2e6a9b93f8539f717fd71e
MANOJ <workwithmonji@gmail.com>
2026-06-15 23:27:41 +0530

    Instances of Resilience 4J retries+cicuitBreaker+ratelimitter

--- order-service/src/main/java/com/monji/ecommerce/order_service/service/OrderService.java
+++ order-service/src/main/java/com/monji/ecommerce/order_service/service/OrderService.java
@@ -6,6 +6,9 @@ import com.monji.ecommerce.order_service.entity.Order;
 import com.monji.ecommerce.order_service.entity.OrderItem;
 import com.monji.ecommerce.order_service.enums.OrderStatus;
 import com.monji.ecommerce.order_service.repository.OrderRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -37,7 +40,11 @@ public class OrderService {
         return modelMapper.map(order, OrderRequestDto.class);
     }
 
+//    @Retry(name = "inventoryRetry", fallbackMethod = "createOrderFallback")
+//    @RateLimiter(name = "inventoryRateLimiter", fallbackMethod = "createOrderFallback")
+    @CircuitBreaker(name = "inventoryCircuitBreaker", fallbackMethod = "createOrderFallback")
     public OrderRequestDto createOrder(OrderRequestDto orderRequestDto) {
+        log.info("Creating order : {}", orderRequestDto);
         Double totalPrice = inventoryFiegnClient.reduceProductStock(orderRequestDto);
 
         // Map order (without items)
@@ -60,5 +67,10 @@ public class OrderService {
 
         Order savedOrder = orderRepository.save(order);
         return modelMapper.map(savedOrder, OrderRequestDto.class);
+    }
+
+    public OrderRequestDto createOrderFallback(OrderRequestDto orderRequestDto, Throwable throwable) {
+        log.error("Creating order fallback due to : {}", throwable.getMessage());
+        return new OrderRequestDto();
     }
 }
